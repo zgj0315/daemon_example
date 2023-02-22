f40d3cb653c7dee6c128f556b0a85bb03ac8e5f3
赵光建 <zhaoguangjian@cyberkl.com>
2023-02-22 16:08:01 +0800

    it works

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,24 +1,35 @@
-use std::{process::Command, thread::sleep, time::Duration};
+use std::process::Command;
 
 use dev_util::log::log_init;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     log_init();
     log::info!("daemon start");
+    let cmds = [
+        "target/debug/app_a",
+        "target/debug/app_b",
+        "target/debug/app_c",
+    ];
+    let mut handles = Vec::new();
+    for cmd in cmds {
+        let handle = tokio::spawn(async move {
+            app(cmd).await;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.await?;
+    }
+    Ok(())
+}
+
+async fn app(cmd: &str) {
     loop {
-        log::info!("app a start");
-        let mut app_a_child = Command::new("target/debug/app_a")
-            .spawn()
-            .expect("failed to execute");
-        log::info!("app a process id: {}", &app_a_child.id());
-        let mut app_b_child = Command::new("target/debug/app_b")
-            .spawn()
-            .expect("failed to execute");
-        log::info!("app b process id: {}", &app_b_child.id());
-        app_a_child.wait().expect("command wasn't running");
-        log::info!("app a end");
-        app_b_child.wait().expect("command wasn't running");
-        log::info!("app b end");
-        sleep(Duration::from_secs(1));
+        log::info!("app {} start", cmd);
+        let mut app_child = Command::new(cmd).spawn().expect("failed to execute");
+        log::info!("app process id: {}", &app_child.id());
+        app_child.wait().expect("command wasn't running");
+        log::warn!("app {} end", cmd);
     }
 }
